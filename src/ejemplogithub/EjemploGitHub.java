047faf323b38b7c488ec96c38825f26e42c12646
gvpujos <67333612+gvpujos@users.noreply.github.com>
2020-06-23 11:45:29 -0500

    Update EjemploGitHub.java

--- src/ejemplogithub/EjemploGitHub.java
+++ src/ejemplogithub/EjemploGitHub.java
@@ -4,7 +4,7 @@ package ejemplogithub;
 public class EjemploGitHub {
 
     public static void main(String[] args) {
-       System.out.println("Saludos estimados progamadores");
+       System.out.println("Saludos estimados Juan Perez");
     }
     
     
